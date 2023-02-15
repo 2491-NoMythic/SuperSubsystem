062592132239679287ee5a23a647f1b5f27be2cd
2491NoMythic <2491nomythic@gmail.com>
2023-02-14 19:15:59 -0600

    Inital commit

---
+++ src/main/java/frc/robot/subsystems/SubsystemPneumatics.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.PneumaticsControlModule;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsBase;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import frc.robot.Constants;
+import frc.robot.Constants.Pneumatics;
+import edu.wpi.first.wpilibj.Compressor;
+
+
+public class SubsystemPneumatics extends SubsystemBase {
+  /** Creates a new Pneumatics. */
+  private DoubleSolenoid solenoid;
+  private Compressor compressor;
+
+  public SubsystemPneumatics() {
+    compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+    solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Pneumatics.FORWARD_CHANNEL, Constants.Pneumatics.REVERSE_CHANNEL);
+    compressor.enableDigital();
+  }
+
+  public void setForward(){
+    solenoid.set(Value.kForward);
+  }
+
+  public void setReverse(){
+    solenoid.set(Value.kReverse);
+  }
+
+  
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
